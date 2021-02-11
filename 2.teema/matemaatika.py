8b768381aaeded78fc5b3e33c4ecf7c2ae0ef901
Alvar Kõiv <alvar.koiv@khk.ee>
2021-02-11 13:45:50 +0200

    Klassi ja eksemplaari loomise katse

---
+++ 2.teema/matemaatika.py
@@ -0,0 +1,5 @@
+class matemaatika():
+    number = 5
+
+    def liitmine(self, arv):
+        print(matemaatika.number + arv)
---
+++ 2.teema/test.py
@@ -0,0 +1,8 @@
+from matemaatika import matemaatika
+#from file import klass
+a = matemaatika()
+print(a)
+print(a.number)
+a.number = 8
+print(a.number)
+a.liitmine(4)
