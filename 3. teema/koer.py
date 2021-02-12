ccdb89698060101c24f45ec440e7a237a91fbfe8
Alvar Kõiv <alvar.koiv@khk.ee>
2021-02-12 11:22:29 +0200

    Klassi konstruktori atribuutidele konkreetsed väärtused

---
+++ 3. teema/koer.py
@@ -0,0 +1,11 @@
+class Koer():
+    hyydnimi = ""
+    vanus = 0
+
+    def __init__(self, h, v):
+        self.hyydnimi = h
+        self.vanus = v
+
+    def kirjeldus(self):
+
+    format("Koer nimega {} on {} aastat vana", )
